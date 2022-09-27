77605c3a980384adf2dfe0f37ef57048993da690
Odrannn <bernard.mazo04@gmail.com>
2022-09-28 00:03:26 +0800

    sa

--- Barangay System/Admin/case_management/update_complaint.php
+++ Barangay System/Admin/case_management/update_complaint.php
@@ -1,16 +1,21 @@
 <?php
     session_start();
+    include("../../phpfiles/connection.php");
 
     $res_ID = $_SESSION['user_id'];
+    $query = "SELECT official_id FROM tblofficial WHERE resident_id='$res_ID'";
+    $result = $conn -> query($query);
+    $row = mysqli_fetch_array($result);
+    $official_id = $row[0];
     
+    echo $official_id;
     if (isset($_POST['solve'])) {
         $id = $_POST['user_id'];
-        include("../../phpfiles/connection.php");
-
-        
         $query = "UPDATE complaint_table
-        SET complaint_status = 'solved' set
+        SET complaint_status = 'solved', official_ID = '$official_id'
         WHERE complaint_ID = '$id'";
+        echo $official_id;
+        echo $id;
         $result = $conn -> query($query);
 
         header("location:case_management.php");
