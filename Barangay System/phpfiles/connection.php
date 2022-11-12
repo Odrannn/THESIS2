b5a07637621a5f9b76447d0903fe8af7c25a3c52
Wilcent Urbano <82427306+Odrannn@users.noreply.github.com>
2022-11-13 04:23:55 +0800

    sa

--- Barangay System/phpfiles/connection.php
+++ Barangay System/phpfiles/connection.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conn = new mysqli("localhost", "u314764576_itbg", "Yahoocom12", "u314764576_bgy_system") or die("Unable to connect");
+    $conn = new mysqli("localhost", "root", "", "bgy_system") or die("Unable to connect");
 
 ?>
